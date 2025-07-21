0dec09fdd97ef48a91b26b1126a8114d0ebf293e
tayoky <tayoky4848@gmail.com>
2025-07-21 19:45:41 +0200

    tests : fix the sourcing test

--- test/src.sh
+++ test/src.sh
@@ -1,11 +1,10 @@
 # test . built in
 
-# start by generating the script
-echo "#script sourced by the . test" > src
-echo "export TEST=hello" >> src
+if [ "$1" = "test" ] ; then
+	export TEST=hello
+	exit
+fi
 
 export TEST=""
-
-# then source it
-. ./src
+. ./src.sh test
 echo $TEST

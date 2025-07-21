1a9558f3e33befcf03e8637206a0b960a62f50e9
tayoky <tayoky4848@gmail.com>
2025-07-21 22:31:54 +0200

    tests : use bash instead of sh to compare

--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
+
+#compare two s output
+SHELL1=bash
+SHELL2=tash
 cd test
 
 rm -f $(find -not -name "*.sh" -type f)
 
 for TEST in $(ls) ; do
-	sh $TEST > $TEST-sh.out
-	tash $TEST > $TEST-tash.out
-	if diff -s $TEST-tash.out $TEST-sh.out > /dev/null ; then
+	$SHELL1 $TEST > $TEST-$SHELL1.out
+	$SHELL2 $TEST > $TEST-$SHELL2.out
+	if diff -s $TEST-$SHELL1.out $TEST-$SHELL2.out > /dev/null ; then
 		echo $TEST passed
 	else
 		echo $TEST failed
-		diff $TEST-tash.out $TEST-sh.out --color
+		diff $TEST-$SHELL1.out $TEST-$SHELL2.out --color
 		exit 1
 	fi
 done

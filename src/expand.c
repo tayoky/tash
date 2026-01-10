f29ba6c5568b4e24315d2e8679a24c9efeff68a5
tayoky <tayoky4848@gmail.com>
2026-01-10 13:40:48 +0100

    expand : add support for $# $$ and $?

--- src/expand.c
+++ src/expand.c
@@ -5,10 +5,69 @@
 // do word expansion
 
 static int is_dangerous(int c) {
-	return c == ' ' || c == '\t' || c == '\n' || c == CTLESC || c == CTLQUOT || c == '*' || c == '~';
+	return c == CTLESC || c == CTLQUOT;
+}
+
+static int is_special(int c) {
+	return c == ' ' || c == '\t' || c == '\n' || c == '*' || c == '~' || is_dangerous(c);
 }
 
 #define APPEND(c) vector_push_back(dest, (char[]){c})
+
+static int handle_var(vector_t *dest, const char **ptr, int in_quote) {
+	// TODO : bracket support
+	const char *start = *ptr;
+	const char *src = start;
+	const char *value = NULL;
+	char buf[32];
+	switch (*src) {
+	case '$':
+		sprintf(buf, "%ld", (long)getpid());
+		value = buf;
+		break;
+	case '?':
+		sprintf(buf, "%d", exit_status);
+		value = buf;
+		break;
+	case '#':
+		sprintf(buf, "%d", _argc - 1);
+		value = buf;
+		break;
+	// TODO : $@ and $*
+	case '0':
+	case '1': case '2': case '3':
+	case '4': case '5': case '6':
+	case '7': case '8': case '9':;
+		int index = *src - '0';
+		if (index >= _argc) value = NULL;
+		value = _argv[index];
+		break;
+	default:
+		while (isalnum(*src) || *src == '_') {
+			src++;
+		}
+		if (src == start) {
+			APPEND('$');
+			return 0;
+		}
+		char *var = strndup(start, src - start);
+		value = getvar(var);
+		free(var);
+		src--;
+		break;
+	}
+	*ptr = src;
+	if (!value) {
+		// var is unset
+		return 0;
+	}
+	while (*value) {
+		if ((in_quote && is_special(*value)) || is_dangerous(*value)) APPEND(CTLESC);
+		vector_push_back(dest, value);
+		value++;
+	}
+	return 0;
+}
 
 static int var_expansion(vector_t *dest, const char *src) {
 	int in_quote = 0;
@@ -20,29 +79,8 @@ static int var_expansion(vector_t *dest, const char *src) {
 		} else if (*src == CTLQUOT) {
 			in_quote = 1 - in_quote;
 		} else if (*src == '$') {
-			// TODO : bracket support
 			src++;
-			if (*src == '{') {
-			} else {
-				const char *start = src;
-				while (isalnum(*src) || *src == '_') {
-					src++;
-				}
-				if (src == start) {
-					APPEND('$');
-					continue;
-				}
-				char *var = strndup(start, src - start);
-				const char *value = getvar(var);
-				free(var);
-				if (!value) continue;
-				src--;
-				while (*value) {
-					if (in_quote && is_dangerous(*value)) APPEND(CTLESC);
-					vector_push_back(dest, value);
-					value++;
-				}
-			}
+			handle_var(dest, &src, in_quote);
 		} else {
 			vector_push_back(dest, src);
 		}
